a085a9f96fc96dc5aff824dea3fcd646cd2ad8b9
missing-star <1652575963@qq.com>
2019-07-11 18:16:03 +0800

    首页

---
+++ .history/js/index_20190709162618.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    data: {
+
+    },
+    computed: {
+        currentTime: function () {
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+            var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+            var hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+            var minute = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+            var second = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+            return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+        }
+    },
+});
---
+++ .history/js/index_20190711161146.js
@@ -0,0 +1,29 @@
+new Vue({
+    el: '#app',
+    data: {
+        currentTime: '0000-00-00 00:00:00',
+        currentIndex:0
+    },
+    methods: {
+        getCurrentTime: function () {
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+            var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+            var hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+            var minute = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+            var second = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+            this.currentTime = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+        },
+        switchTab:function(index) {
+            this.currentIndex = index == this.currentIndex ? this.currentIndex : index;
+        }
+    },
+    created: function () {
+        var vm = this;
+        vm.getCurrentTime();
+        setInterval(function(){
+            vm.getCurrentTime();
+        }, 1000);
+    }
+});
